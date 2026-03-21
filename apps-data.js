feabeab625914f8a69e281b0df72f8062f51befc
cC724813 <134193183+cC724813@users.noreply.github.com>
2026-03-21 23:00:05 +0800

    Update apps-data.js

--- apps-data.js
+++ apps-data.js
@@ -7,9 +7,10 @@ const APPS_LIST = [
     { id: "pilipala-android", name: "pilipala", url: "https://loadly.io/pilipalaapk", platform: "android", category: "apps" },
     { id: "pilipala-ios", name: "pilipala", url: "https://loadly.io/pilipalaipa", platform: "ios", category: "apps" },
     { id: "oldbili", name: "旧版bili", url: "https://loadly.io/oldbiliipa", platform: "ios", category: "apps" },
-    { id: "troll", name: "TrollInstallerX", url: "https://loadly.io/Trollipa", platform: "ios", category: "apps" },
+    { id：“troll”， 名称 ：“TrollInstallerX”， 网址 ：“https://loadly.io/Trollipa”， 平台 ：“ios”， 分类 ：“apps” }，
 
     // 游戏（games）
     { id: "gtasa-ios", name: "侠盗猎车手：圣安地列斯", url: "https://loadly.io/gtasaipag", platform: "ios", category: "games" },
     { id: "gtasa-android", name: "侠盗猎车手：圣安地列斯", url: "https://loadly.io/gtasaapkg", platform: "android", category: "games" },
- ];
+    { id: "minecraft-ios", name: "我的世界", url: "https://loadly.io/Minecraftgipa", platform: "ios", category: "games" }, 
+];
