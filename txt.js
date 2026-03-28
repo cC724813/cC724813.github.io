e9fc90fdd588c2a83d39a3ca1fc951d5d7da79fd
cC724813 <134193183+cC724813@users.noreply.github.com>
2026-03-28 13:39:39 +0800

    Add files via upload

---
+++ txt.js
@@ -0,0 +1,11 @@
+// 小说数据存储文件
+const TXT_BOOKS = [
+    { i: "longzu", n: "龙族", u: "wY6AX8DZ" },
+    { i: "douluodalu", n: "斗罗大陆", u: "OVLzLDXM" },
+    { i: "guzhenren", n: "蛊真人", u: "eCgqUkQ9" },
+    { i: "qiangfengchuifu", n: "强风吹拂", u: "Mzsz3jCG" }
+];
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = TXT_BOOKS;
+}
