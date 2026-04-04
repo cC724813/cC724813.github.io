af9bccb0008ba3ca9c834843a993162cec1a32fd
cC724813 <134193183+cC724813@users.noreply.github.com>
2026-04-04 12:29:10 +0800

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,4 @@
+const MOVIE_DATA = [
+    { id: "peaky_blinders", title: "浴血黑帮：不朽传奇", type: "movie", episodes: [{ name: "正片", url: "https://loadly.io/s0iDipdr" }] },
+    { id: "love_apartment", title: "爱情公寓3", type: "tv", episodes: [{ name: "第1集", url: "https://loadly.io/1pX6nJPo" }, { name: "第2集", url: "https://loadly.io/9U0Natvv" }] }
+];
