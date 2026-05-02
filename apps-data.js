d8154200d4c3f2fee4b9ea34eb5c8b90eb5df989
cC724813 <134193183+cC724813@users.noreply.github.com>
2026-05-02 16:55:32 +0800

    Add files via upload

---
+++ apps-data.js
@@ -0,0 +1,19 @@
+const APPS_LIST = [
+    { i: "soutijing", n: "搜题酱", u: "soutijing", p: "i", c: "a" },
+    { i: "oldtiktok", n: "旧版抖音", u: "oldtiktokipa", p: "i", c: "a" },
+    { i: "biliwear", n: "biliwear", u: "biliwearapk", p: "a", c: "a" },
+    { i: "shizuku", n: "shizuku", u: "shizukumapk", p: "a", c: "a" },
+    { i: "pilipala-android", n: "pilipala", u: "pilipalaapk", p: "a", c: "a" },
+    { i: "pilipala-ios", n: "pilipala", u: "pilipalaipa", p: "i", c: "a" },
+    { i: "oldbili", n: "旧版bili", u: "oldbiliipa", p: "i", c: "a" },
+    { i: "troll", n: "TrollInstallerX", u: "Trollipa", p: "i", c: "a" },
+    { i: "pilipius", n: "PiliPlus", u: "ZesgWep9", p: "a", c: "a" },
+    { i: "handnote-reader", n: "手记 (GT Fit阅读器)", u: "mPkecEbW", p: "a", c: "a" },
+    { i: "afusekt", n: "AfuseKt", u: "FJw0UOgj", p: "a", c: "a" },
+    { i: "huawei-health", n: "华为运动健康", u: "xb2Bi9dy", p: "a", c: "a" },
+    { i: "gtasa-ios", n: "侠盗猎车手：圣安地列斯", u: "gtasaipag", p: "i", c: "g" },
+    { i: "gtasa-android", n: "侠盗猎车手：圣安地列斯", u: "gtasaapkg", p: "a", c: "g" },
+    { i: "minecraft-ios", n: "我的世界", u: "Minecraftgipa", p: "i", c: "g" },
+    { i: "terraria", n: "泰拉瑞亚", u: "terraria", p: "i", c: "g" },
+    { i: "stardewvalley", n: "星露谷物语", u: "stardewvalley", p: "i", c: "g" }
+];
