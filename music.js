afdcb1ba656a880771976dc2720ee15998aaac3d
cC724813 <134193183+cC724813@users.noreply.github.com>
2026-06-27 13:11:09 +0800

    Add files via upload

---
+++ music.js
@@ -0,0 +1,16 @@
+var PLAYLIST = [
+    {
+        id: 1,
+        title: '晴天',
+        artist: '周杰伦',
+        mp3: 'nAunprts',
+        lyric: 'FzcG2rVJ'
+    },
+    {
+        id: 2,
+        title: '天地龙鳞',
+        artist: '王力宏',
+        mp3: 'dtAegZHe',
+        lyric: 'LMGAYdz8'
+    }
+];
