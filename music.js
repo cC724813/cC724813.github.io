3a318d53500ebf28d8d6eefaa8d5c24ce832fcab
cC724813 <134193183+cC724813@users.noreply.github.com>
2026-06-27 10:17:34 +0800

    Add files via upload

---
+++ music.js
@@ -0,0 +1,18 @@
+var PLAYLIST = [
+    {
+        id: 1,
+        title: '晴天',
+        artist: '周杰伦',
+        mp3: 'https://loadly.io/nAunprts',
+        lyric: 'https://loadly.io/FzcG2rVJ',
+        cover: null
+    },
+    {
+        id: 2,
+        title: '天地龙鳞',
+        artist: '王力宏',
+        mp3: 'https://loadly.io/dtAegZHe',
+        lyric: 'https://loadly.io/LMGAYdz8',
+        cover: null
+    }
+];
