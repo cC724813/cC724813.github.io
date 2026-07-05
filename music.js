0e39cc3f6d6eb06a6ebe6a92b4b77b57cd55b104
cC724813 <134193183+cC724813@users.noreply.github.com>
2026-07-06 01:22:35 +0800

    Add files via upload

---
+++ music.js
@@ -0,0 +1,65 @@
+var PLAYLIST = [
+    {
+        id: 1,
+        title: '晴天',
+        artist: '周杰伦',
+        mp3: 'nAunprts',
+        lyric: 'FzcG2rVJ'
+    },
+    {
+        id: 2,
+        title: '天地龙鳞',
+        artist: '王力宏',
+        mp3: 'dtAegZHe',
+        lyric: 'LMGAYdz8'
+    },
+    {
+        id: 3,
+        title: '天若有情',
+        artist: 'A-Lin',
+        mp3: 'Je9Tarru',
+        lyric: 'YV7yejGG'
+    },
+    {
+        id: 4,
+        title: '红尘客栈',
+        artist: '周杰伦',
+        mp3: 'LzWemGXt',
+        lyric: 'QxAk02SK'
+    },
+    {
+        id: 5,
+        title: '去年夏天',
+        artist: '王大毛',
+        mp3: 'hwNfVNFA',
+        lyric: 'HjHAo6X3'
+    },
+    {
+        id: 6,
+        title: '兰亭序',
+        artist: '周杰伦',
+        mp3: '3sFjRC4K',
+        lyric: 'ePX4Xs4t'
+    },
+    {
+        id: 7,
+        title: '恋曲1990',
+        artist: '罗大佑',
+        mp3: 'nwRseNQX',
+        lyric: 'TpqfiUym'
+    },
+    {
+        id: 8,
+        title: "Baby, Don't Cry (人鱼的眼泪)",
+        artist: 'EXO',
+        mp3: '3rnhHw1f',
+        lyric: 'B8esjH6L'
+    },
+    {
+        id: 9,
+        title: '青花瓷',
+        artist: '周杰伦',
+        mp3: 'eBOszpHC',
+        lyric: '4Gvp6AQY'
+    }
+];
