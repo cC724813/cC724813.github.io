55b8ec3d4fd1ff7a899e936b2bae4e1af70e6d49
cC724813 <134193183+cC724813@users.noreply.github.com>
2026-07-23 18:05:15 +0800

    Add files via upload

---
+++ music.js
@@ -0,0 +1,156 @@
+var PLAYLIST = [
+    {
+        id: 1,
+        title: '晴天',
+        artist: '周杰伦',
+        mp3: 'nAunprts',
+        lyric: 'FzcG2rVJ'
+    },
+    {
+        id: 2,
+        title: '天地龙鳞',
+        artist: '王力宏',
+        mp3: 'dtAegZHe',
+        lyric: 'LMGAYdz8'
+    },
+    {
+        id: 3,
+        title: '天若有情',
+        artist: 'A-Lin',
+        mp3: 'Je9Tarru',
+        lyric: 'YV7yejGG'
+    },
+    {
+        id: 4,
+        title: '红尘客栈',
+        artist: '周杰伦',
+        mp3: 'LzWemGXt',
+        lyric: 'QxAk02SK'
+    },
+    {
+        id: 5,
+        title: '去年夏天',
+        artist: '王大毛',
+        mp3: 'hwNfVNFA',
+        lyric: 'HjHAo6X3'
+    },
+    {
+        id: 6,
+        title: '兰亭序',
+        artist: '周杰伦',
+        mp3: '3sFjRC4K',
+        lyric: 'ePX4Xs4t'
+    },
+    {
+        id: 7,
+        title: '恋曲1990',
+        artist: '罗大佑',
+        mp3: 'nwRseNQX',
+        lyric: 'TpqfiUym'
+    },
+    {
+        id: 8,
+        title: "Baby, Don't Cry (人鱼的眼泪)",
+        artist: 'EXO',
+        mp3: '3rnhHw1f',
+        lyric: 'B8esjH6L'
+    },
+    {
+        id: 9,
+        title: '青花瓷',
+        artist: '周杰伦',
+        mp3: 'eBOszpHC',
+        lyric: '4Gvp6AQY'
+    },
+    {
+        id: 10,
+        title: 'Come Back To Me',
+        artist: '宇多田ヒカル',
+        mp3: 'giudFkKp',
+        lyric: 'BCjx4xXa'
+    },
+    {
+        id: 11,
+        title: 'I Really Want to Stay At Your House (粤语改编版)',
+        artist: '銘晴Halo',
+        mp3: '3nwn6Z3j',
+        lyric: 'eb6TDnXo'
+    },
+    {
+        id: 12,
+        title: '7 Years',
+        artist: 'Lukas Graham',
+        mp3: '2vSksvtp',
+        lyric: 'oCE2UsSd'
+    },
+    {
+        id: 13,
+        title: '大眠',
+        artist: '王心凌',
+        mp3: 'ywMOG7Rw',
+        lyric: '81ycaMOY'
+    },
+    {
+        id: 14,
+        title: '鼓楼',
+        artist: '赵雷',
+        mp3: 'k6MpkeNt',
+        lyric: 'b8GXgbKV'
+    },
+    {
+        id: 15,
+        title: '心墙',
+        artist: '林俊杰',
+        mp3: 'wF87cnpT',
+        lyric: 'LpJ0t7Tr'
+    },
+    {
+        id: 16,
+        title: '心墙',
+        artist: '郭静',
+        mp3: 'vOEpkkL6',
+        lyric: 'TDXsGNDa'
+    },
+    {
+        id: 17,
+        title: '我怀念的',
+        artist: '孙燕姿',
+        mp3: 'w9jCnV33',
+        lyric: 'CahwEshL'
+    },
+    {
+        id: 18,
+        title: '樱花草',
+        artist: 'Sweety',
+        mp3: 'ZGC8Ko6h',
+        lyric: '7403fNQj'
+    },
+    {
+        id: 19,
+        title: 'ANGEL',
+        artist: '윤미래&Tiger JK&비지',
+        mp3: 'UxCHTfjS',
+        lyric: '9A5pzEDM'
+    },
+    {
+        id: 20,
+        title: '把回忆拼好给你',
+        artist: 'cici_',
+        mp3: 'AH1gdJsh',
+        lyric: 'v7qj3UfX'
+    },
+    {
+        id: 21,
+        title: '老男孩',
+        artist: '筷子兄弟',
+        mp3: 'U8zPydii',
+        lyric: 't5kmWnkZ'
+    },
+    {
+        id: 22,
+        title: '认真的雪',
+        artist: '薛之谦',
+        mp3: 'SJuJ2Vka',
+        lyric: '4UJMi7c5'
+    }
+];
