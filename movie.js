99e3f33bbfeb736c9b02d36bb32b4d43bfe38ee6
cC724813 <134193183+cC724813@users.noreply.github.com>
2026-07-26 13:49:49 +0800

    Add files via upload

---
+++ movie.js
@@ -0,0 +1,80 @@
+const MOVIE_DATA = [
+    { 
+        id: "peaky_blinders", 
+        title: "浴血黑帮：不朽传奇", 
+        type: "movie", 
+        episodes: [{ name: "正片", url: "s0iDipdr" }] 
+    },
+    { 
+        id: "love_apartment", 
+        title: "爱情公寓3", 
+        type: "tv", 
+        episodes: [
+            { name: "第1集", url: "1pX6nJPo" },
+            { name: "第2集", url: "9U0Natvv" },
+            { name: "第3集", url: "iPz1NuGm" },
+            { name: "第4集", url: "snxYoYH0" },
+            { name: "第5集", url: "AXyGw6GP" },
+            { name: "第6集", url: "hyqWhaQr" },
+            { name: "第7集", url: "4zkQM1Cu" },
+            { name: "第8集", url: "K15B2Xun" },
+            { name: "第9集", url: "cgNP0pVq" },
+            { name: "第10集", url: "O7M6wxPE" },
+            { name: "第11集", url: "YBNkOaX5" },
+            { name: "第12集", url: "C6znAA3t" },
+            { name: "第13集", url: "bqkmPq0S" },
+            { name: "第14集", url: "t7Yv2sbo" },
+            { name: "第15集", url: "YRXWsKeU" },
+            { name: "第16集", url: "qLbSkJx0" },
+            { name: "第17集", url: "Q0U2xXVs" },
+            { name: "第18集", url: "0kw91jbE" },
+            { name: "第19集", url: "rPNbYtXF" },
+            { name: "第20集", url: "ouAEmaCU" },
+            { name: "第21集", url: "Kry1CdKC" },
+            { name: "第22集", url: "HCqSLUnt" },
+            { name: "第23集", url: "s4L4izyu" },
+            { name: "第24集", url: "ReszXCWG" }
+        ] 
+    },
+    { 
+        id: "yi_yu_zhi_mo_lu_ying_xiong", 
+        title: "异域之末路英雄", 
+        type: "movie", 
+        episodes: [{ name: "正片", url: "CcDHUztf" }] 
+    },
+    { 
+        id: "yi_yu", 
+        title: "异域", 
+        type: "movie", 
+        episodes: [{ name: "正片", url: "yMSBQPER" }] 
+    },
+    { 
+        id: "huozhe", 
+        title: "活着", 
+        type: "movie", 
+        episodes: [{ name: "正片", url: "bK5WAzQA" }] 
+    },
+    { 
+        id: "qi_wang", 
+        title: "棋王", 
+        type: "movie", 
+        episodes: [{ name: "正片", url: "FKUKyf7t" }] 
+    },
+    { 
+        id: "sd-gundam-sanguozhuan", 
+        title: "SD高达三国传", 
+        type: "tv", 
+        episodes: [
+            { name: "第1集", url: "1rL0JHd3" },
+            { name: "第2集", url: "d328DtsW" },
+            { name: "第3集", url: "T7ky9tRF" },
+            { name: "第4集", url: "eiEWHn3w" },
+            { name: "第5集", url: "TRGKbqcN" },
+            { name: "第6集", url: "1yfytn7s" },
+            { name: "第7集", url: "Zekf18ny" },
+            { name: "第8集", url: "jAO70Hs8" },
+            { name: "第9集", url: "87ycBVeo" },
+            { name: "第10集", url: "5coZWuKx" }
+        ] 
+    }
+];
